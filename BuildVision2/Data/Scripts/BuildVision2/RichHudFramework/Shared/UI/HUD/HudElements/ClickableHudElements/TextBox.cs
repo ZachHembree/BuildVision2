61b8bb27109425201dd8dde6c4a092ef1c2473ec
Zach Hembree <zhembree654@gmail.com>
2020-01-16 19:15:23 -0500

    Updated client subrepo

--- BuildVision2/Data/Scripts/BuildVision2/RichHudFramework/Shared/UI/HUD/HudElements/ClickableHudElements/TextBox.cs
+++ BuildVision2/Data/Scripts/BuildVision2/RichHudFramework/Shared/UI/HUD/HudElements/ClickableHudElements/TextBox.cs
@@ -405,7 +405,7 @@ namespace RichHudFramework.UI
             /// <summary>
             /// If true, then the current selection is empty.
             /// </summary>
-            public bool Empty => top == null || (Start == -Vector2I.One || End == -Vector2I.One);
+            public bool Empty => top == null || Start == End;
 
             private readonly TextCaret caret;
             private readonly ITextBoard text;
@@ -518,7 +518,7 @@ namespace RichHudFramework.UI
                 float leftBound = left.Offset.X,
                     rightBound = right.Offset.X + right.Size.X / 2f;
 
-                if (prepend && left.Offset != right.Offset)
+                if (prepend)
                     leftBound += left.Size.X / 2f;
                 else
                     leftBound -= left.Size.X / 2f;
