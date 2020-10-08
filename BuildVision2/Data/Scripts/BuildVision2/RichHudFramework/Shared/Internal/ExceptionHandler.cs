6dbbda2b177316b7c7733a0f1c5990174d4574ee
Zach Hembree <zhembree654@gmail.com>
2020-10-08 13:12:14 -0400

    Move IsClient/Server/Dedicated properties into ExceptionHandler class

--- BuildVision2/Data/Scripts/BuildVision2/RichHudFramework/Shared/Internal/ExceptionHandler.cs
+++ BuildVision2/Data/Scripts/BuildVision2/RichHudFramework/Shared/Internal/ExceptionHandler.cs
@@ -47,6 +47,21 @@ namespace RichHudFramework.Internal
         /// </summary>
         public static bool Unloading { get; private set; }
 
+        /// <summary>
+        /// If true, the mod is currently running on a client.
+        /// </summary>
+        public static bool IsClient { get; private set; }
+
+        /// <summary>
+        /// If true, the mod is currently running on a server.
+        /// </summary>
+        public static bool IsServer { get; private set; }
+
+        /// <summary>
+        /// If true, the mod is currently running on a dedicated server.
+        /// </summary>
+        public static bool IsDedicated { get; private set; }
+
         public static bool ClientsPaused { get; private set; }
 
         private static ExceptionHandler instance;
@@ -73,6 +88,13 @@ namespace RichHudFramework.Internal
             exceptionMessages = new List<string>();
             errorTimer = new Utils.Stopwatch();
             clients = new List<ModBase>();
+        }
+
+        public override void LoadData()
+        {
+            IsDedicated = MyAPIGateway.Utilities.IsDedicated;
+            IsServer = MyAPIGateway.Session.OnlineMode == MyOnlineModeEnum.OFFLINE || MyAPIGateway.Multiplayer.IsServer || IsDedicated;
+            IsClient = !IsDedicated;
         }
 
         /// <summary>
@@ -185,7 +207,7 @@ namespace RichHudFramework.Internal
                 LogIO.TryWriteToLog(ModName + " encountered an unhandled exception.\n" + exceptionText + '\n');
                 exceptionMessages.Clear();
 
-                if (ModBase.IsClient && PromptForReload)
+                if (IsClient && PromptForReload)
                 {
                     if (RecoveryAttempts < RecoveryLimit)
                     {
@@ -297,7 +319,7 @@ namespace RichHudFramework.Internal
         /// </summary>
         public static void SendChatMessage(string message)
         {
-            if (!ModBase.IsDedicated)
+            if (!IsDedicated)
             {
                 try
                 {
