7af6b0fdcf08b660bfdda4a4ddfd6ad9fca1ebe8
Zach Hembree <zhembree654@gmail.com>
2022-02-02 20:06:33 -0500

    Remove falloff property from light accessor
    Reading from this property is weirdly unreliable, so I'm just going to remove it.

--- BuildVision2/Data/Scripts/BuildVision2/Terminal/BlockData/SuperBlock/Subtypes/LightAccessor.cs
+++ BuildVision2/Data/Scripts/BuildVision2/Terminal/BlockData/SuperBlock/Subtypes/LightAccessor.cs
@@ -22,17 +22,12 @@ namespace DarkHelmet.BuildVision2
             /// <summary>
             /// Controls the light's color
             /// </summary>
-        public Color Color { get { return subtype.Color; } set { subtype.Color = value; } }
+            public Color Color { get { return subtype.Color; } set { subtype.Color = value; } }
 
             /// <summary>
             /// Controls the light's intensity
             /// </summary>
             public float Intensity { get { return subtype.Intensity; } set { subtype.Intensity = value; } }
-
-            /// <summary>
-            /// Controls the light's falloff
-            /// </summary>
-            public float Falloff { get { return subtype.Falloff; } set { subtype.Falloff = value; } }
 
             /// <summary>
             /// Controls the lighting radius
@@ -77,16 +72,6 @@ namespace DarkHelmet.BuildVision2
 
                 block.textBuffer.Clear();
                 TerminalUtilities.GetDistanceDisplay(Radius, block.textBuffer);
-                block.textBuffer.Append('\n');
-
-                builder.Add(block.textBuffer, valueFormat);
-
-                // Light falloff
-                builder.Add(MyTexts.GetString(MySpaceTexts.BlockPropertyTitle_LightFalloff), nameFormat);
-                builder.Add(": ", nameFormat);
-
-                block.textBuffer.Clear();
-                block.textBuffer.Append(Math.Round(Falloff, 2));
                 block.textBuffer.Append('\n');
 
                 builder.Add(block.textBuffer, valueFormat);
