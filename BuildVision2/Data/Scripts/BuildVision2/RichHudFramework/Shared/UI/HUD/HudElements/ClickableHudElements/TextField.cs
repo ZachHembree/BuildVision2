7f88cf66f1516b4f4b290d995f1a97f30d0e3ca7
Zach Hembree <zhembree654@gmail.com>
2022-02-13 07:44:32 -0500

    Prevent TextBox from drawing carat for OOB chars + fix exceptions
    Opening input will also now move the text offset to the character selected

--- BuildVision2/Data/Scripts/BuildVision2/RichHudFramework/Shared/UI/HUD/HudElements/ClickableHudElements/TextField.cs
+++ BuildVision2/Data/Scripts/BuildVision2/RichHudFramework/Shared/UI/HUD/HudElements/ClickableHudElements/TextField.cs
@@ -162,11 +162,12 @@ namespace RichHudFramework.UI
             UseFocusFormatting = true;
             HighlightEnabled = true;
 
-            Size = new Vector2(319f, 40);
+            Size = new Vector2(250f, 40);
 
             textBox.TextBoard.TextChanged += OnTextChanged;
             MouseInput.CursorEntered += CursorEnter;
             MouseInput.CursorExited += CursorExit;
+            MouseInput.GainedInputFocus += GainFocus;
             MouseInput.LostInputFocus += LoseFocus;
         }
 
@@ -182,6 +183,15 @@ namespace RichHudFramework.UI
         private void OnTextChanged()
         {
             TextChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        protected override void HandleInput(Vector2 cursorPos)
+        {
+            if (!MouseInput.HasFocus && !MouseInput.IsMousedOver)
+            {
+                lastColor = Color;
+                lastTextColor = TextBoard.Format.Color;
+            }
         }
 
         protected virtual void CursorEnter(object sender, EventArgs args)
@@ -213,6 +223,21 @@ namespace RichHudFramework.UI
                     Color = lastColor;
                     TextBoard.SetFormatting(TextBoard.Format.WithColor(lastTextColor));
                 }
+            }
+        }
+
+        protected virtual void GainFocus(object sender, EventArgs args)
+        {
+            if (UseFocusFormatting)
+            {
+                if (!MouseInput.IsMousedOver)
+                {
+                    lastColor = Color;
+                    lastTextColor = TextBoard.Format.Color;
+                }
+
+                Color = FocusColor;
+                TextBoard.SetFormatting(TextBoard.Format.WithColor(FocusTextColor));
             }
         }
 
