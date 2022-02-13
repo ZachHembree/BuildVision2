0b6b7cccca6e33fe8958d264fbe0100391d3552e
Zach Hembree <zhembree654@gmail.com>
2022-02-13 12:10:20 -0500

    Fix float widget text entry

--- BuildVision2/Data/Scripts/BuildVision2/Terminal/QuickActionMenu/PropertyWheelMenu/Widgets/FloatWidget.cs
+++ BuildVision2/Data/Scripts/BuildVision2/Terminal/QuickActionMenu/PropertyWheelMenu/Widgets/FloatWidget.cs
@@ -1,5 +1,6 @@
 ﻿using RichHudFramework.UI;
 using RichHudFramework.UI.Client;
+using RichHudFramework.UI.Rendering;
 using System;
 using VRageMath;
 
@@ -9,19 +10,15 @@ namespace DarkHelmet.BuildVision2
     {
         private class FloatWidget : BlockValueWidgetBase
         {
-            private readonly TextField textField;
-            private readonly NamedSliderBox sliderBox;
+            private readonly CustomSliderBox sliderBox;
 
             private IBlockNumericValue<float> floatMember;
             private IBlockTextMember textMember;
             private float initValue;
-            private bool useManualEntry;
 
             public FloatWidget(HudParentBase parent = null) : base(parent)
             {
-                textField = new TextField() { Visible = false };
-                sliderBox = new NamedSliderBox() { Padding = Vector2.Zero };
-                useManualEntry = false;
+                sliderBox = new CustomSliderBox() { Padding = Vector2.Zero };
 
                 var layout = new HudChain(true, this)
                 {
@@ -30,7 +27,6 @@ namespace DarkHelmet.BuildVision2
                     Spacing = 8f,
                     CollectionContainer =
                     {
-                        { textField, true },
                         sliderBox,
                         buttonChain
                     }
@@ -48,15 +44,22 @@ namespace DarkHelmet.BuildVision2
                 sliderBox.Current = floatMember.Value;
                 sliderBox.NameBuilder.SetText(floatMember.Name);
 
-                textField.CharFilterFunc = textMember.CharFilterFunc;
+                sliderBox.MouseInput.GetInputFocus();
+                sliderBox.CharFilterFunc = textMember.CharFilterFunc;
                 initValue = floatMember.Value;
+
+                if (BindManager.IsChatOpen && !sliderBox.IsTextInputOpen)
+                {
+                    sliderBox.FieldText.SetText(textMember.ValueText);
+                    sliderBox.OpenTextInput();
+                }
             }
 
             public override void Reset()
             {
                 floatMember = null;
                 CloseWidgetCallback = null;
-                CloseFieldInput();
+                sliderBox.CloseTextInput();
             }
 
             protected override void Layout()
@@ -66,16 +69,21 @@ namespace DarkHelmet.BuildVision2
 
             protected override void HandleInput(Vector2 cursorPos)
             {
-                if (!useManualEntry)
+                if (SharedBinds.Enter.IsNewPressed)
+                {
+                    if (BindManager.IsChatOpen && !sliderBox.IsTextInputOpen)
+                    {
+                        sliderBox.FieldText.SetText(textMember.ValueText);
+                        sliderBox.OpenTextInput();
+                    }
+                    else if (!BindManager.IsChatOpen && sliderBox.IsTextInputOpen)
+                        Confirm();
+                }
+                else if (!sliderBox.IsTextInputOpen)
                 {
                     floatMember.Value = sliderBox.Current;
 
-                    if (BindManager.IsChatOpen && !useManualEntry)
-                        OpenFieldInput();
-                    else if (!BindManager.IsChatOpen && useManualEntry)
-                        Confirm();
-
-                    if (!useManualEntry && (BvBinds.ScrollUp.IsNewPressed || BvBinds.ScrollDown.IsNewPressed))
+                    if (!sliderBox.IsTextInputOpen && (BvBinds.ScrollUp.IsNewPressed || BvBinds.ScrollDown.IsNewPressed))
                     {
                         float offset = floatMember.Increment;
 
@@ -102,14 +110,14 @@ namespace DarkHelmet.BuildVision2
 
             protected override void Confirm()
             {
-                if (useManualEntry)
+                if (sliderBox.IsTextInputOpen)
                 {
                     float newValue;
 
-                    if (float.TryParse(textField.TextBoard.ToString(), out newValue))
+                    if (float.TryParse(sliderBox.FieldText.ToString(), out newValue))
                         floatMember.Value = newValue;
 
-                    CloseFieldInput();
+                    sliderBox.CloseTextInput();
                 }
 
                 CloseWidgetCallback?.Invoke();
@@ -117,35 +125,69 @@ namespace DarkHelmet.BuildVision2
 
             protected override void Cancel()
             {
-                if (!useManualEntry)
+                if (!sliderBox.IsTextInputOpen)
                 {
                     floatMember.Value = initValue;
                 }
                 else
                 {
-                    CloseFieldInput();
+                    sliderBox.CloseTextInput();
                 }
 
                 CloseWidgetCallback?.Invoke();
             }
 
-            private void OpenFieldInput()
+            private class CustomSliderBox : NamedSliderBox
             {
-                textField.Visible = true;
-                sliderBox.Visible = false;
+                public bool IsTextInputOpen => textField.InputOpen;
 
-                textField.TextBoard.SetText(floatMember.ValueText);
-                textField.OpenInput();
+                public ITextBuilder FieldText => textField.TextBoard;
 
-                useManualEntry = true;
-            }
+                public Func<char, bool> CharFilterFunc 
+                { 
+                    get { return textField.CharFilterFunc; } 
+                    set { textField.CharFilterFunc = value; } 
+                }
 
-            private void CloseFieldInput()
-            {
-                textField.Visible = false;
-                sliderBox.Visible = true;
-                useManualEntry = false;
-                textField.CloseInput();
+                private readonly TextField textField;
+
+                public CustomSliderBox(HudParentBase parent = null) : base(parent)
+                {
+                    textField = new TextField()
+                    {
+                        Height = 47f,
+                        DimAlignment = DimAlignments.Width | DimAlignments.IgnorePadding,
+                        ParentAlignment = ParentAlignments.Bottom | ParentAlignments.InnerV,
+                        Visible = false,
+                    };
+                    textField.Register(this, true);
+                }
+
+                public void OpenTextInput()
+                {
+                    if (!IsTextInputOpen)
+                    {
+                        sliderBox.Visible = false;
+                        textField.Visible = true;
+                        current.Visible = false;
+
+                        textField.OpenInput();
+                        textField.MouseInput.GetInputFocus();
+                    }
+                }
+
+                public void CloseTextInput()
+                {
+                    if (IsTextInputOpen)
+                    {
+                        current.Visible = true;
+                        sliderBox.Visible = true;
+                        textField.Visible = false;
+
+                        textField.CloseInput();
+                        sliderBox.MouseInput.GetInputFocus();
+                    }
+                }
             }
         }
     }
