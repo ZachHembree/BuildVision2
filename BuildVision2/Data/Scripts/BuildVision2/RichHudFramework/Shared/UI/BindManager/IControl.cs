70953349a1e94001dcf7a9dc481768cbd61df70f
Zach Hembree <zhembree654@gmail.com>
2022-02-18 06:26:40 -0500

    git subrepo pull BuildVision2/Data/Scripts/BuildVision2/RichHudFramework
    subrepo:
      subdir:   "BuildVision2/Data/Scripts/BuildVision2/RichHudFramework"
      merged:   "fe72eae"
    upstream:
      origin:   "git@github.com:ZachHembree/RichHudFramework.Client.git"
      branch:   "master"
      commit:   "fe72eae"
    git-subrepo:
      version:  "0.4.3"
      origin:   "???"
      commit:   "???"

--- BuildVision2/Data/Scripts/BuildVision2/RichHudFramework/Shared/UI/BindManager/IControl.cs
+++ BuildVision2/Data/Scripts/BuildVision2/RichHudFramework/Shared/UI/BindManager/IControl.cs
@@ -27,6 +27,16 @@
             /// </summary>
             bool IsPressed { get; }
 
+            /// <summary>
+            /// Returns true if the control was just pressed
+            /// </summary>
+            bool IsNewPressed { get; }
+
+            /// <summary>
+            /// Returns true if the control was just released
+            /// </summary>
+            bool IsReleased { get; }
+
             // <summary>
             /// Returns true if the control doesn't represent a boolean value. For example, MwUp/Dn
             /// represent scroll wheel movement, but don't return an exact position/displacement.
@@ -59,7 +69,17 @@
             /// <summary>
             /// out: bool
             /// </summary>
-            Analog = 5
+            Analog = 5,
+
+            /// <summary>
+            /// out: bool
+            /// </summary>
+            IsNewPressed = 6,
+
+            /// <summary>
+            /// out: bool
+            /// </summary>
+            IsReleased = 7
         }
     }
 }
