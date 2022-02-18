6762dd60d0f207f1b9c7b9d62a375293b4990a50
Zach Hembree <zhembree654@gmail.com>
2022-02-18 06:15:06 -0500

    Update help text

--- BuildVision2/Data/Scripts/BuildVision2/Input/HelpText.cs
+++ BuildVision2/Data/Scripts/BuildVision2/Input/HelpText.cs
@@ -21,58 +21,64 @@ namespace DarkHelmet.BuildVision2
         {
             return new RichText
             {
-                { $"To open Build Vision, aim at the block you want to control and press press " }, { $"[{GetBindString(BvBinds.OpenWheel)}]", highlight }, {"; to close the menu, " },
-                { $"[{GetBindString(BvBinds.OpenList)}]", highlight }, { ". Pressing and holding " }, {$"[{GetBindString(BvBinds.EnableMouse)}]", highlight}, 
-                {" will allow you to use the mouse.\n\n" },
+                $"This is a quick access context menu for changing block settings, opened by aiming at the desired block and pressing either ",
+                {$"[{GetBindString(BvBinds.OpenWheel)}]", highlight}, " or ", {$"[{GetBindString(BvBinds.OpenList)}].", highlight}, " The first " +
+                "opens the wheel menu; the second opens the older list-style menu. Holding ", { $"[{GetBindString(BvBinds.MultX)}]", highlight },
+                $" (peek) will show a summary of the target block's current status without opening the controls.\n\n",
 
-                { $"[{GetBindString(BvBinds.ScrollUp)}]", highlight }, { $" and " }, { $"[{GetBindString(BvBinds.ScrollDown)}]", highlight }, 
-                { " can be used to scroll up and down the list and to increment / decrement numerical properties when selected. To select a property in the menu press " },
-                { $"[{GetBindString(BvBinds.Select)}].\n\n", highlight },
+                $"Mouse input can be enabled for the wheel menu by holding the ", {"Peek", highlight }, " bind. Alternatively, you can use the same " +
+                "scrolling + multiplier controls listed below  that are shared with the list menu. By default, the menu will close if you move more than " +
+                "10 meters ", {"(4 large blocks)", highlight }, " from your target block. The exact distance can be customized in the settings section.\n\n",
 
-                { $"By default, the menu will close if you move more than 10 meters " }, {"(4 large blocks)", highlight }, {" from your target block. The exact distance can " },
-                { $"be customized in the settings section.\n\n" },
+                { $"Main Binds:\n\n", subsection },
 
-                { $"Main Binds:\n\n", subsection},
+                $"\tOpen Wheel:\t\t|\t[{GetBindString(BvBinds.OpenWheel)}]\n",
+                $"\tOpen List:\t\t|\t[{GetBindString(BvBinds.OpenList)}]\n",
+                $"\tSelect/Confirm:\t|\t[{GetBindString(BvBinds.Select)}]\n",
+                $"\tCancel/Back:\t|\t[{GetBindString(BvBinds.Cancel)}]\n",
+                $"\tScroll Up:\t\t\t|\t[{GetBindString(BvBinds.ScrollUp)}]\n",
+                $"\tScroll Down:\t\t|\t[{GetBindString(BvBinds.ScrollDown)}]\n\n",
 
-                { $"\tOpen Menu:\t\t|\t[{GetBindString(BvBinds.OpenWheel)}]\n" },
-                { $"\tClose Menu:\t\t|\t[{GetBindString(BvBinds.OpenList)}]\n" },
-                { $"\tSelect:\t|\t[{GetBindString(BvBinds.Select)}]\n" },
-                { $"\tScroll Up:\t\t\t|\t[{GetBindString(BvBinds.ScrollUp)}]\n" },
-                { $"\tScroll Down:\t\t|\t[{GetBindString(BvBinds.ScrollDown)}]\n\n" },
+                { $"Modifiers:\n", subsection},
 
-                { $"Multiplier Binds:\n", subsection},
+                $"The multiplier binds are used to change the ", {"speed", highlight }, " at which a selected property will change with each tick of the scroll wheel, ",
+                { $"1/10th", highlight }, $" normal, ", {$"5x", highlight }, $" normal, ", {$"10x", highlight }, $", etc.\n\n",
 
-                { $"The multiplier binds are used to change the " }, {"speed", highlight }, {" at which a selected property will change with each tick of the scroll wheel, " },
-                { $"1/10th", highlight }, {$" normal, " }, {$"5x", highlight }, {$" normal, " }, {$"10x", highlight }, {$", etc.\n\n" },
+                $"\tMultX/Peek (x{BvConfig.Current.block.floatMult.X}):\t|\t[{GetBindString(BvBinds.MultX)}]\n",
+                $"\tMultY (x{BvConfig.Current.block.floatMult.Y}):\t\t\t|\t[{GetBindString(BvBinds.MultY)}]\n",
+                $"\tMultZ (x{BvConfig.Current.block.floatMult.Z}):\t\t\t|\t[{GetBindString(BvBinds.MultZ)}]\n\n",
 
-                { $"\tMultX (x{BvConfig.Current.block.floatMult.X}):\t\t|\t[{GetBindString(BvBinds.MultX)}]\n" },
-                { $"\tMultY (x{BvConfig.Current.block.floatMult.Y}):\t\t|\t[{GetBindString(BvBinds.MultY)}]\n" },
-                { $"\tMultZ (x{BvConfig.Current.block.floatMult.Z}):\t\t|\t[{GetBindString(BvBinds.MultZ)}]\n\n" },
+                $"The MultX bind, Ctrl by default, can also be used to scroll through the list ", {"faster", highlight }, ". Just hold it down while scrolling.\n\n",
 
-                { $"The MultX bind, Ctrl by default, can also be used to scroll through the list " }, {"faster", highlight }, {". Just hold it down while scrolling.\n\n" },
+                { $"Property Duplication:\n", subsection},
+
+                $"Property or settings duplication, as the name implies, is used to copy block settings from one block to another. The controls below can be used to " +
+                $"quickly switch between dupe mode and normal control, but there are buttons for these controls in the wheel menu as well.\n\n",
+
+                $"\tStart Dupe:\t\t|\t[{GetBindString(BvBinds.StartDupe)}]\n",
+                $"\tStope Dupe:\t\t|\t[{GetBindString(BvBinds.StopDupe)}]\n\n",
 
                 { $"Settings Menu:\n", subheader},
-                { $"By default, this settings menu can be toggled by pressing " }, { "F1", highlight }, { " while having chat open. From here, you can configure block " },
-                { $"targeting, change UI settings and configure your keybinds.\n\n" },
+                $"By default, this settings menu can be toggled by pressing ", { "F2", highlight }, " From here, you can configure block ",
+                $"targeting, change UI settings and configure your keybinds.\n\n",
 
                 { $"Chat Commands:\n", subheader},
-                { $"The chat commands in this mod are largely redundant; the functionality they provide is also provided by the settings menu. " },
-                { $"If you prefer to use the chat commands for whatever reason, here they are:\n\n" },
+                $"The chat commands in this mod are largely redundant; the functionality they provide is also provided by the settings menu. ",
+                $"If you prefer to use the chat commands for whatever reason, here they are:\n\n",
 
-                { $"All chat commands must begin with " }, { "“/bv2”", highlight }, { "and are not case-sensitive. The arguments following " }, { "“/bv2”", highlight }, 
-                { " can be separated either by whitespace, a comma, semicolon or pipe character. Whatever floats your boat; just make sure there’s something between them.\n\n" },
+                $"All chat commands must begin with ", { "“/bv2”", highlight }, "and are not case-sensitive. The arguments following ", { "“/bv2”", highlight }, 
+                " can be separated either by whitespace, a comma, semicolon or pipe character. Whatever floats your boat; just make sure there’s something between them.\n\n",
 
-                { $"• help:\t\t\tYou are here.\n" },
-                { $"• printBinds:\t\tPrints your current key binds to chat.\n" },
-                { $"• save:\t\t\tSaves the current configuration\n" },
-                { $"• load:\t\t\tLoads configuration from the config file\n" },
-                { $"• resetBinds:\t\tResets all keybinds\n" },
-                { $"• resetConfig:\tResets all settings to default\n" },
-                { $"• bind\t\t\t\t[bindName] [control1] [control2] [control3]\n\n" },
+                $"• help:\t\t\tYou are here.\n",
+                $"• save:\t\t\tSaves the current configuration\n",
+                $"• load:\t\t\tLoads configuration from the config file\n",
+                $"• resetBinds:\t\tResets all keybinds\n",
+                $"• resetConfig:\tResets all settings to default\n",
+                $"• bind\t\t\t\t[bindName] [control1] [control2] [control3]\n\n",
 
-                { $"Example: \"/bv2 bind open control shift\"\n\n" },
+                $"Example: \"/bv2 bind open control shift\"\n\n",
 
-                { $"For more information, see the Build Vision 2 workshop page." }
+                $"For more information, see the Build Vision workshop page."
             };
         }
 
