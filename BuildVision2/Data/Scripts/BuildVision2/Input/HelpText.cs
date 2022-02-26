241a33fe8ca7b94c11385448cd1c12e4c3bcfbef
Zach Hembree <zhembree654@gmail.com>
2022-02-26 17:42:27 -0500

    Update help text

--- BuildVision2/Data/Scripts/BuildVision2/Input/HelpText.cs
+++ BuildVision2/Data/Scripts/BuildVision2/Input/HelpText.cs
@@ -14,33 +14,33 @@ namespace DarkHelmet.BuildVision2
         private static readonly Color accentGrey = new Color(200, 200, 210);
         private static readonly GlyphFormat 
             highlight = new GlyphFormat(accentGrey),
-            subheader = new GlyphFormat(accentGrey, TextAlignment.Center, 1.2f, FontStyles.Underline),
-            subsection = new GlyphFormat(accentGrey, style: FontStyles.Underline);
+            subheader = new GlyphFormat(accentGrey, TextAlignment.Center, 1.1f, FontStyles.Underline),
+            subsection = new GlyphFormat(QuickActionMenu.highlightFormat.Color, style: FontStyles.Underline);
 
         public static RichText GetHelpMessage()
         {
             return new RichText(GlyphFormat.White)
             {
-                { $"Update 3.0:\n\n", subsection },
+                { " Update 3.0:\n\n", subheader },
 
                 $"This update brings a complete redesign of Build Vision's UI and control scheme. The functionality is the same as it was " +
                 $"in 2.5, but the old list menu has been (mostly) replaced with an easier-to-use wheel menu.\n\n",
                 
-                $"The list menu is still available for properties not in the wheel menu, or if you simply prefer it. See the description " +
-                $"below for the new binds and usage.\n\n",
+                $"The list menu is still available for properties not in the wheel menu and the old control scheme can be restored using the " +
+                $"Legacy Mode option in the settings. See the description below for the new binds and usage.\n\n",
 
-                { $"Usage:\n\n", subsection },
+                { "Usage \n\n", subheader },
 
-                $"This is a quick access context menu for changing block settings, opened by aiming at the desired block and pressing either ",
-                {$"[{GetBindString(BvBinds.OpenWheel)}]", highlight}, " or ", {$"[{GetBindString(BvBinds.OpenList)}].", highlight}, " The first " +
-                "opens the wheel menu; the second opens the older list-style menu. Holding ", { $"[{GetBindString(BvBinds.MultX)}]", highlight },
-                $" (peek) will show a summary of the target block's current status without opening the controls.\n\n",
+                $"This menu is opened by aiming at the desired block and pressing either ", {$"[{GetBindString(BvBinds.OpenWheel)}]", highlight}, 
+                " or ", {$"[{GetBindString(BvBinds.OpenList)}].", highlight}, " The first opens the wheel menu; the second opens the older " +
+                "list-style menu. Holding ", { $"[{GetBindString(BvBinds.MultX)}]", highlight }, $" (peek) will show a summary of the target " +
+                $"block's current status without opening the controls. Pressing ", {$"[{GetBindString(BvBinds.Cancel)}]", highlight}, "(Back/Cancel) " +
+                "will close the menu.\n\n",
 
-                $"Mouse input can be enabled for the wheel menu by holding the ", {"Peek", highlight }, " bind. Alternatively, you can use the same " +
-                "scrolling + multiplier controls listed below that are shared with the list menu. By default, the menu will close if you move more than " +
-                "10 meters ", {"(4 large blocks)", highlight }, " from your target block. The exact distance can be customized in the settings section.\n\n",
+                $"By default, the menu will close if you move more than  10 meters ", {"(4 large blocks)", highlight }, " from your target block. " +
+                "The exact distance can be customized in the settings section.\n\n",
 
-                { $"Main Binds:\n\n", subsection },
+                { "Main Binds \n\n", subheader },
 
                 $"\tOpen Wheel:\t\t|\t[{GetBindString(BvBinds.OpenWheel)}]\n",
                 $"\tOpen List:\t\t|\t[{GetBindString(BvBinds.OpenList)}]\n",
@@ -49,7 +49,32 @@ namespace DarkHelmet.BuildVision2
                 $"\tScroll Up:\t\t\t|\t[{GetBindString(BvBinds.ScrollUp)}]\n",
                 $"\tScroll Down:\t\t|\t[{GetBindString(BvBinds.ScrollDown)}]\n\n",
 
-                { $"Modifiers:\n", subsection},
+                { "Selection:\n", subsection},
+                "There are two ways to select properties: scrolling or by moving the mouse. To highlight with the mouse, hold ",
+                { $"[{GetBindString(BvBinds.MultX)}]", highlight }, " (Peek), and move your mouse either clockwise or counterclockwise, like you're " +
+                "tracing a circle.\n\n",
+
+                "Pressing ", { "Select/Confirm", highlight }, " will open the highlighted property. Pressing that bind again will confirm any changes." +
+                "Pressing ", { "Back/Cancel", highlight }, " will close the property without saving changes; pressing it again will close the menu.\n\n",
+
+                { "Note:", highlight }, " The list menu only supports scrolling with the mouse wheel (or scroll binds).\n\n",
+
+                { "Changing Settings:\n", subsection},
+                "Properties can be changed using the scroll wheel in conjunction with the multiplier binds listed below, with text-only fields being the " +
+                "only exception.\n\n",
+               
+                "Alternatively, ",{ "Peek", highlight }, " will allow you to use your cursor in the wheel menu with the control in the center. Depending on " +
+                "the use case, this is often easier.\n\n",
+
+                { "Text:\n", subsection},
+                "Text fields are opened/closed with chat (Enter) and support the usage of the usual Ctrl+A/X/C/V binds to select, cut, copy and paste text. " +
+                "Fair warning: you'll only be able to copy text within Build Vision; it won't work with the terminal or anything else.\n\n" +
+
+                "Opening chat (pressing Enter) while a text field is highlighted will open text input automatically.\n\n",
+
+                { "Note:", highlight }, " Most numerical fields support text input.\n\n",
+
+                { "Modifiers \n", subheader},
 
                 $"The multiplier binds are used to change the ", {"speed", highlight }, " at which a selected property will change with each tick of the scroll wheel, ",
                 { $"1/10th", highlight }, $" normal, ", {$"5x", highlight }, $" normal, ", {$"10x", highlight }, $", etc.\n\n",
@@ -60,19 +85,28 @@ namespace DarkHelmet.BuildVision2
 
                 $"The MultX bind, Ctrl by default, can also be used to scroll through the list ", {"faster", highlight }, ". Just hold it down while scrolling.\n\n",
 
-                { $"Property Duplication:\n", subsection},
+                { $"Property Duplication Shortcuts \n", subheader},
 
                 $"Property or settings duplication, as the name implies, is used to copy block settings from one block to another. The controls below can be used to " +
-                $"quickly switch between dupe mode and normal control, but there are buttons for these controls in the wheel menu as well.\n\n",
+                $"quickly copy block settings, but there are buttons for these controls in the wheel menu as well.\n\n",
 
                 $"\tStart Dupe:\t\t|\t[{GetBindString(BvBinds.StartDupe)}]\n",
-                $"\tStope Dupe:\t\t|\t[{GetBindString(BvBinds.StopDupe)}]\n\n",
+                $"\tStop Dupe:\t\t|\t[{GetBindString(BvBinds.StopDupe)}]\n",
+                $"\tToggle Dupe:\t\t|\t[{GetBindString(BvBinds.ToggleDupe)}]\n",
+                $"\tSelect All:\t\t|\t[{GetBindString(BvBinds.SelectAll)}]\n",
+                $"\tCopy Selection:\t|\t[{GetBindString(BvBinds.CopySelection)}]\n",
+                $"\tPaste Copy:\t\t|\t[{GetBindString(BvBinds.PasteProperties)}]\n",
+                $"\tUndo Paste:\t\t|\t[{GetBindString(BvBinds.UndoPaste)}]\n\n",
 
-                { $"Settings Menu:\n", subheader},
+                "When selecting properties from the list, pressing the back bind will take you back to the wheel menu to finish copying.\n\n",
+
+                { "Settings Menu:\n", subheader},
+
                 $"By default, this settings menu can be toggled by pressing ", { "F2.", highlight }, " From here, you can configure block ",
                 $"targeting, change UI settings and configure your keybinds.\n\n",
 
-                { $"Chat Commands:\n", subheader},
+                { "Chat Commands:\n", subheader},
+
                 $"The chat commands in this mod are largely redundant; the functionality they provide is also provided by the settings menu. ",
                 $"If you prefer to use the chat commands for whatever reason, here they are:\n\n",
 
