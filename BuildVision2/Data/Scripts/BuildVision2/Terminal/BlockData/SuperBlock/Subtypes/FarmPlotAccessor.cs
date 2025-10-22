1462a5f32c55114ea4db288472e0d8030b6c9c34
Zach Hembree <zhembree654@gmail.com>
2025-10-22 07:02:57 -0400

    Add farm plot grow time and water usage to peek summary

--- BuildVision2/Data/Scripts/BuildVision2/Terminal/BlockData/SuperBlock/Subtypes/FarmPlotAccessor.cs
+++ BuildVision2/Data/Scripts/BuildVision2/Terminal/BlockData/SuperBlock/Subtypes/FarmPlotAccessor.cs
@@ -1,12 +1,8 @@
 ﻿using RichHudFramework.UI;
-using Sandbox.Game.Components;
-using Sandbox.Game.EntityComponents;
+using Sandbox.Definitions;
 using Sandbox.ModAPI;
-using System;
-using System.Text;
 using VRage;
-using VRage.Game.Entity.UseObject;
-using VRage.Utils;
+using VRage.Game;
 using MySpaceTexts = Sandbox.Game.Localization.MySpaceTexts;
 
 namespace DarkHelmet.BuildVision2
@@ -19,20 +15,47 @@ namespace DarkHelmet.BuildVision2
 
 		public class FarmPlotAccessor : ComponentAccessor<IMyFarmPlotLogic>
 		{
+			/// <summary>
+			/// Returns true if the plant is alive
+			/// </summary>
 			public bool IsAlive => component.IsAlive;
 
+			/// <summary>
+			/// Returns true if the plant has finished growing
+			/// </summary>
 			public bool IsGrown => component.IsPlantFullyGrown;
 
+			/// <summary>
+			/// Returns true if a plant is in the plot
+			/// </summary>
 			public bool IsPlanted => component.IsPlantPlanted;
 
 			/// <summary>
-			/// Detailed info subsection for planter info
+			/// Returns the name of the plant in the plot
+			/// </summary>
+			public string PlantName => plantDef?.DisplayNameText ?? MyTexts.GetString(MySpaceTexts.None);
+
+			/// <summary>
+			/// Detailed info from plant management screen
 			/// </summary>
 			public string PlanterInfo => component.GetDetailedInfoWithoutRequiredInput();
+
+			private MyDefinitionBase plantDef;
 
 			public override void SetBlock(SuperBlock block)
 			{
 				base.SetBlock(block, TBlockSubtypes.FarmPlot);
+
+				if (component != null && component.OutputItem != default(MyDefinitionId))
+				{
+					plantDef = MyDefinitionManager.Static.GetDefinition(component.OutputItem);
+				}
+			}
+
+			public override void Reset()
+			{
+				base.Reset();
+				plantDef = null;
 			}
 
 			public void Harvest()
@@ -43,20 +66,45 @@ namespace DarkHelmet.BuildVision2
 			public override void GetSummary(RichText builder, GlyphFormat nameFormat, GlyphFormat valueFormat)
 			{
 				string detailedInfo = component.GetDetailedInfoWithoutRequiredInput();
+
+				/*
+					Need: Crop Type, Growth Progress, Grow Time, Crop Health, Water Level and Usage
+
+					Components to investigate:
+					MyResourceSinkComponent
+					MyResourceSourceComponent
+					MyResourceStorageComponent
+				 */
+
+				// Crop type
+				builder.Add(MyTexts.GetString(MySpaceTexts.BlockPropertyProperties_CurrentCropType), nameFormat);
+				builder.Add(PlantName, valueFormat);
+				builder.Add('\n');
+
+				// Growth Progres
 				var textBuf = block.textBuffer;
 				textBuf.Clear();
 
-				// Crop type
-				if (TerminalUtilities.TryParseValueFromInfoString(detailedInfo, MySpaceTexts.BlockPropertyProperties_CurrentCropType, textBuf))
+				if (TerminalUtilities.TryParseValueFromInfoString(detailedInfo, MySpaceTexts.BlockPropertyProperties_GrowthProgress, textBuf))
 				{
-					builder.Add(MyTexts.GetString(MySpaceTexts.BlockPropertyProperties_CurrentCropType), nameFormat);
+					builder.Add(MyTexts.GetString(MySpaceTexts.BlockPropertyProperties_GrowthProgress), nameFormat);
 					textBuf.Append('\n');
 					builder.Add(textBuf, valueFormat);
 				}
 
+				// Grow Time
 				textBuf.Clear();
 
-				// Crop health
+				if (TerminalUtilities.TryParseValueFromInfoString(detailedInfo, MySpaceTexts.BlockPropertyProperties_GrowTime, textBuf))
+				{
+					builder.Add(MyTexts.GetString(MySpaceTexts.BlockPropertyProperties_GrowTime), nameFormat);
+					textBuf.Append('\n');
+					builder.Add(textBuf, valueFormat);
+				}
+
+				// Crop Health
+				textBuf.Clear();
+
 				if (TerminalUtilities.TryParseValueFromInfoString(detailedInfo, MySpaceTexts.BlockPropertyProperties_CropHealth, textBuf))
 				{
 					builder.Add(MyTexts.GetString(MySpaceTexts.BlockPropertyProperties_CropHealth), nameFormat);
@@ -64,12 +112,22 @@ namespace DarkHelmet.BuildVision2
 					builder.Add(textBuf, valueFormat);
 				}
 
+				// Water Level
 				textBuf.Clear();
 
-				// Water level
 				if (TerminalUtilities.TryParseValueFromInfoString(detailedInfo, MySpaceTexts.BlockPropertyProperties_WaterLevel, textBuf))
 				{
 					builder.Add(MyTexts.GetString(MySpaceTexts.BlockPropertyProperties_WaterLevel), nameFormat);
+					textBuf.Append('\n');
+					builder.Add(textBuf, valueFormat);
+				}
+
+				// Water Usage 
+				textBuf.Clear();
+
+				if (TerminalUtilities.TryParseValueFromInfoString(detailedInfo, MySpaceTexts.BlockPropertyProperties_WaterUsage, textBuf))
+				{
+					builder.Add(MyTexts.GetString(MySpaceTexts.BlockPropertyProperties_WaterUsage), nameFormat);
 					textBuf.Append('\n');
 					builder.Add(textBuf, valueFormat);
 				}
